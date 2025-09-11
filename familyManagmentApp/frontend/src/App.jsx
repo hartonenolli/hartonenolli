172bd2bccc54413c516c8fc762b7f372f3662d87
hartonenolli <hartonenolli@gmail.com>
2025-09-11 14:45:55 +0300

    Added calendar to the project

--- familyManagmentApp/frontend/src/App.jsx
+++ familyManagmentApp/frontend/src/App.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from 'react'
+import { Calendar as BigCalendar, dateFnsLocalizer } from 'react-big-calendar'
+import { format, parse, startOfWeek, getDay } from 'date-fns'
+import enUS from 'date-fns/locale/en-US'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 const App = () => {
   const [messageFromBackend, setMessageFromBackend] = useState('')
@@ -49,10 +53,35 @@ const App = () => {
     }))
   }
 
+  const Calendar = () => {
+    const locales = { 'en-US': enUS }
+    const localizer = dateFnsLocalizer({
+      format,
+      parse,
+      startOfWeek: () => startOfWeek(new Date(), { weekStartsOn: 1 }),
+      getDay,
+      locales,
+    })
+    return (
+      <div>
+        <h2>Calendar Component</h2>
+        <BigCalendar
+          localizer={localizer}
+          events={[]}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 400 }}
+        />
+      </div>
+    )
+  }
+
   return (
     <div className="container">
       <p>Hello world</p>
       <p>Message from backend: {messageFromBackend}</p>
+      <Calendar />
+      <h2>Add New Person</h2>
       <form onSubmit={handleAddPerson}>
         <input
           type="text"
