2c4dd170d5f7e3921055ac2a6c96e39a1f0b7407
hartonenolli <hartonenolli@gmail.com>
2025-09-15 09:27:50 +0300

    Fronend feches the weather data from backend and shows it to the user

--- familyManagmentApp/frontend/src/components/Calendar.jsx
+++ familyManagmentApp/frontend/src/components/Calendar.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react'
 import { Calendar as BigCalendar, dateFnsLocalizer } from 'react-big-calendar'
 import { format, parse, startOfWeek, getDay } from 'date-fns'
 import enUS from 'date-fns/locale/en-US'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import axios from 'axios'
+
 
 const Calendar = ({ events }) => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const response = await axios.get('/api/weather?city=Helsinki');
+      setWeatherData(response.data);
+      console.log('Weather data response:', response.data);
+    };
+    fetchWeatherData();
+  }, [events])
+
   const locales = { 'en-US': enUS }
   const localizer = dateFnsLocalizer({
     format,
@@ -13,11 +27,32 @@ const Calendar = ({ events }) => {
     locales,
   })
 
-  console.log('Events passed to Calendar:', events)
+
+  const WeatherForToday = () => {
+    if (!weatherData) {
+      return <div>Loading weather data...</div>;
+    }
+
+    return (
+      <div>
+        <h3>Weather Forecast for Today in Helsinki</h3>
+        <ul>
+          <li>Temperature: {weatherData.current.temp_c} °C</li>
+          <li>Condition: {weatherData.current.condition.text}</li>
+          <li>Humidity: {weatherData.current.humidity} %</li>
+          <li>Wind: {weatherData.current.wind_kph} kph</li>
+          <li>
+            <img src={weatherData.current.condition.icon} alt="Weather icon" />
+          </li>
+        </ul>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h2>Calendar Component</h2>
+      <WeatherForToday />
+      <h2>Calendar</h2>
       <BigCalendar
         localizer={localizer}
         events={events}
